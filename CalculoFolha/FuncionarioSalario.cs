1deb62963e061eff24321cac029c3a137abf3b86
Ruan Blotta <ruanblotta@gmail.com>
2024-03-26 18:53:46 -0300

    Adicionado Orientação ao Objeto. Adicionado classe FuncionarioSalario

---
+++ CalculoFolha/FuncionarioSalario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoFolha
+{
+    public class FuncionarioSalario
+    {
+        public string Nome;
+        public double Salario;
+        public double Imposto;
+       
+
+        public double CalcSalarioLiquido()
+        {
+            return Salario - (Salario * (Imposto / 100));
+        }
+
+        public void CalcNovoSalario(double aumento)
+        {
+            Salario = Salario + (Salario * aumento / 100);
+        }
+    }
+}
--- CalculoFolha/Program.cs
+++ CalculoFolha/Program.cs
@@ -6,46 +6,31 @@ namespace CalculoFolha
     {
         static void Main(string[] args)
         {
-            string nome;
-            double salario, imposto;
-
+            FuncionarioSalario funcionario = new FuncionarioSalario();
+            
             Console.WriteLine("Informe o funcinário: ");
-            nome = Console.ReadLine();
+            funcionario.Nome = Console.ReadLine();
 
             Console.WriteLine("Informe o salário bruto: ");
-            salario = double.Parse(Console.ReadLine());
+            funcionario.Salario = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Informe o índice de imposto: ");
-            imposto = double.Parse(Console.ReadLine());
+            funcionario.Imposto = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Nome:" + nome);
+            Console.WriteLine("Nome:" + funcionario.Nome);
 
-            double salarioLiquido = CalcSalarioLiquido(salario, imposto);
+            double salarioLiquido = funcionario.CalcSalarioLiquido();
             Console.WriteLine("Salário Líquido: " + salarioLiquido);
 
             Console.WriteLine("Informe o índice de aumento: ");
             double taxaAumento = double.Parse(Console.ReadLine());
 
-            double salarioNovo = CalcAumento(salario, taxaAumento);
+            funcionario.CalcNovoSalario(taxaAumento);
 
-            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Nome: " + funcionario.Nome);
 
-            salarioLiquido = CalcSalarioLiquido(salarioNovo, imposto);
+            salarioLiquido = funcionario.CalcSalarioLiquido();
             Console.WriteLine("Salário Líquido: " + salarioLiquido);
-        }
-
-        public static double CalcSalarioLiquido (double salario, double imposto)
-        {
-            double salarioLiquido = salario - (salario * (imposto / 100));
-
-            return salarioLiquido;
-        }
-
-        public static double CalcAumento(double salario, double aumento)
-        {
-            double novoSalario = salario + (salario * aumento / 100);
-
-            return novoSalario;
         }
     }
 }
